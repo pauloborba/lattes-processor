f110697efcbef3ce1c6d1eb464a6db48355c8812
João Felix <jvbf@cin.ufpe.br>
2020-11-12 14:46:06 -0300

    add pesquisadorFactory signature

---
+++ server/pesquisadorFactory.ts
@@ -0,0 +1,9 @@
+import { Pesquisador } from '../common/pesquisador';
+import { Pesquisadores } from './pesquisadores';
+ 
+export class LattesFactory {
+    pesquisadores: Pesquisadores;
+    
+    getObjetoFabricado(xml: string): Pesquisador {
+    }
+}
