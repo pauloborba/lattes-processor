d6ffe1b1d0b6d58e111e2cf9c9804cdd545f6fa5
João Victor de Barros Felix <30508696+jvbfelix@users.noreply.github.com>
2020-11-12 22:52:42 -0300

    Update pesquisadorFactory.ts

--- server/pesquisadorFactory.ts
+++ server/pesquisadorFactory.ts
@@ -1,8 +1,6 @@
 import { Pesquisador } from '../common/pesquisador';
-import { Pesquisadores } from './pesquisadores';
  
 export class LattesFactory {
-    pesquisadores: Pesquisadores;
     
     getObjetoFabricado(xml: string): Pesquisador {
     }
