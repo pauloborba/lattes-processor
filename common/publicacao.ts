4ea92844be3d5c867139f302e5df6e464838e785
Bernardo <bsgam@cin.ufpe.br>
2020-11-13 15:45:55 -0300

    Ajustes esqueleto

--- common/publicacao.ts
+++ common/publicacao.ts
@@ -3,16 +3,7 @@ import { Veiculo } from './veiculo';
 export class Publicacao{
 
 	 titulo : string;
-	 autores : string;
+	 autores : string[];
 	 veiculo =  Veiculo;
-
-	adicionar(Pub:string,id:string) : boolean{
-
-	}
-
-	remover(id:string) : boolean {
-
-	}
-
 
 }
