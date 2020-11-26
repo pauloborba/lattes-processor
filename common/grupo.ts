3a157baf68c6acfc62c83b2f73c07674378cad41
Mikael <mvs2@cin.ufpe.br>
2020-11-26 12:10:02 -0300

    Estruturas basicas do servidor para INSERIR, REMOVER ou ATUALIZAR GRUPOS

--- common/grupo.ts
+++ common/grupo.ts
@@ -1,28 +1,27 @@
 import { Pesquisador } from './pesquisador';
-
+ 
 export class Grupo {
-	private nomeGrupo: String;
-	private pesquisadores : Pesquisador[] = [];
+	 nomeGrupo: string;
+	 pesquisadores : Pesquisador[] = [];
+
 
 	constructor() {
 		this.clean();
-	  }
-
+	}
+	
 	clean(): void {
 		this.nomeGrupo = "";
 	}
-	
+ 
 	 clone(): Grupo {
 		var grupo: Grupo = new Grupo();
 		grupo.copyFrom(this);
 		return grupo;
 	}
-
+ 
 	copyFrom(from: Grupo): void {
 		this.nomeGrupo = from.nomeGrupo;
 	}
-
+ 
 }
-
-
-
+ 
--- server/cadastrogrupos.ts
+++ server/cadastrogrupos.ts
@@ -1,19 +1,120 @@
 import { Grupo } from  '../common/grupo';
 import { Pesquisador } from '../common/pesquisador';
-
+ 
 export class CadastroGrupos {
 	grupo: Grupo[] = [];
 
-	adicionar(grupo: Grupo): boolean {
-		return;
-	}
+	/*start_grupo(){
+		let new_grupo = new Grupo();
+		
+		let pesq: Pesquisador = new Pesquisador();
+        pesq.Nome = 'Joao';
+		pesq.Cpf = '11111111111';
+		new_grupo.pesquisadores.push(pesq);
+       	pesq = new Pesquisador();
+       	pesq.Nome = "Michael";
+       	pesq.Cpf = "22222222222";
+       	new_grupo.pesquisadores.push(pesq);
+	
+       	new_grupo.nomeGrupo = "Voxar"   	   
+		this.grupo.push(new_grupo);
+		
+		new_grupo = new Grupo();
+		pesq = new Pesquisador();
+        pesq.Nome = 'Joao';
+		pesq.Cpf = '11111111111';
+		new_grupo.pesquisadores.push(pesq);
+       	pesq = new Pesquisador();
+       	pesq.Nome = "Michael";
+       	pesq.Cpf = "22222222222";
+       	new_grupo.pesquisadores.push(pesq);
+	
+       	new_grupo.nomeGrupo = "GRUPO 2"   	   
+		this.grupo.push(new_grupo);
 
+		new_grupo = new Grupo();
+		pesq = new Pesquisador();
+        pesq.Nome = 'asadsd';
+		pesq.Cpf = '5565665665';
+		new_grupo.pesquisadores.push(pesq);
+       	pesq = new Pesquisador();
+       	pesq.Nome = "DFSFFFSD";
+       	pesq.Cpf = "899898989";
+       	new_grupo.pesquisadores.push(pesq);
+	
+       	new_grupo.nomeGrupo = "Grupo de Pesquisa"   	   
+		this.grupo.push(new_grupo);
+	}*/
+ 
+	adicionar(b: Grupo): boolean {
+		if(1){this.grupo.push(b);
+ 
+		return true;
+		}
+		else return false;
+	}
+ 
 	remover(nomeGrupo: String): boolean {
-		return;
+		/*for (let i of this.grupo){
+			if(i.nomeGrupo == nomeGrupo){
+				let index = this.grupo.indexOf(i,0);
+				this.grupo.splice(index,1);
+				return true;
+			}
+		}*/
+		this.grupo = this.grupo.filter(item => item.nomeGrupo !== nomeGrupo);
+		return true;
 	}
-
-	atualizar(pesquisador: Pesquisador): Pesquisador{
-		return;
+ 
+	atualizar(tipo: string, a: String, pesquisador: Pesquisador[]): boolean{
+		//Seleciono a chave especifica e atualizo os valores
+		if(tipo == "ADICIONAR"){
+			for(let i of this.grupo){
+				
+				if(i.nomeGrupo == a){
+					let obj = this.grupo.indexOf(i, 0);
+					for(let j of pesquisador){
+						this.grupo[obj].pesquisadores.push(j);
+					}
+					
+					return true;
+				}
+			}
+		}
+		else if(tipo == "REMOVER"){
+			for(let i of this.grupo){
+				console.log("--" + i.nomeGrupo);
+				console.log(i.pesquisadores);
+				if(i.nomeGrupo == a){
+				  console.log(i.nomeGrupo);
+				  //let pesq = new Pesquisador();
+				 
+				  for(let j of i.pesquisadores){
+					//console.log("+ " + j.Nome);
+					for(let k of pesquisador){
+					  //console.log("//" + k );
+					  if(j.Nome == k.Nome){
+						let objindex = i.pesquisadores.indexOf(j,0);
+						i.pesquisadores.splice(objindex,1);
+						//console.log("- -- " + pesquisaro);
+					  }
+					}
+				   
+				  }
+				  //console.log(i.pesquisadores);
+				  //console.log(pesquisador);
+				  //console.log(i.pesquisadores);
+				}
+			  
+			  }
+			
+			return true;
+		}
+		else{
+			return false;
+		}
+		
 	}
-
+	
+ 
 }
